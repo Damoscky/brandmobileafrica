72a17afa6cf7e517ad4247e90afbdec33144ef09
Akinwunmi Damilola <adamilola@sbsc.com>
2020-11-11 23:51:16 +0100

    Choice Models

---
+++ database/migrations/2020_11_11_224037_create_questions_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateQuestionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('questions', function (Blueprint $table) {
+            $table->id();
+            $table->mediumText('question')->nullable();
+            $table->integer('category_id')->nullable();
+            $table->boolean('is_general')->default(true);
+            $table->integer('point')->nullable();
+            $table->mediumText('icon_url')->nullable();
+            $table->integer('duration')->nullable();
+            $table->boolean('is_active')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('questions');
+    }
+}
---
+++ database/migrations/2020_11_11_224734_create_choices_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateChoicesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('choices', function (Blueprint $table) {
+            $table->id();
+            $table->integer('question_id');
+            $table->string('description')->nullable();
+            $table->boolean('is_correct_choice')->default(false);
+            $table->mediumText('icon_url')->nullable();
+            $table->boolean('is_active')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('choices');
+    }
+}
