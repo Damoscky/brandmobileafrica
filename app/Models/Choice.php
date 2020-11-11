0d627f5f9b89d2734144e44934c34e583573eec8
Akinwunmi Damilola <adamilola@sbsc.com>
2020-11-11 23:54:58 +0100

    Relationships

--- app/Models/Choice.php
+++ app/Models/Choice.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Choice extends Model
 {
-    //
+    protected $guarded = [''];
+
+    /** RELATIONSHIP **/
+    public function question()
+    {
+        return $this->belongsTo(Question::class);
+    }
 }
--- app/Models/Question.php
+++ app/Models/Question.php
@@ -6,5 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Question extends Model
 {
-    //
+    protected $guarded = [''];
+
+    /** RELATIONSHIP **/
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function choice()
+    {
+        return $this->hasMany(Choice::class);
+    }
+
 }
